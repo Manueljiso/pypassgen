324335dadfbb7a9f5f1235f18d7dd1f5d4425fb4
ManuelJS <manuel.jiso@outlook.com>
2023-06-18 22:16:28 -0600

    GUI elements rearranged and special characters default values added

--- passgen.py
+++ passgen.py
@@ -28,46 +28,38 @@ def copy_to_clipboard():
 root = tk.Tk()
 root.title("Password Generator")
 
-# Length selection
-length_label = tk.Label(root, text="Password Length:")
-length_label.grid(row=0, column=0, padx=5, pady=5)
-length_entry = ttk.Entry(root)
-length_entry.insert(0, "10")  # Default length
-length_entry.grid(row=0, column=1, padx=5, pady=5)
-
-# Uppercase checkbox
-uppercase_var = tk.IntVar()
-uppercase_checkbox = ttk.Checkbutton(root, text="Include Uppercase", variable=uppercase_var)
-uppercase_checkbox.grid(row=1, column=0, padx=5, pady=5)
-
-# Special characters checkbox
-special_chars_var = tk.IntVar()
-special_chars_checkbox = ttk.Checkbutton(root, text="Include Special Characters", variable=special_chars_var)
-special_chars_checkbox.grid(row=2, column=0, padx=5, pady=5)
-
-# Special characters entry
-special_chars_entry = ttk.Entry(root)
-special_chars_entry.grid(row=2, column=1, padx=5, pady=5)
-special_chars_entry.config(state="disabled")
-
-def toggle_special_chars():
-    if special_chars_var.get():
-        special_chars_entry.config(state="normal")
-    else:
-        special_chars_entry.config(state="disabled")
-
-special_chars_checkbox.config(command=toggle_special_chars)
-
-# Generate password button
-generate_button = ttk.Button(root, text="Generate Password", command=generate_password)
-generate_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
-
 # Password label
-password_label = ttk.Label(root, text="")
-password_label.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+password_label = ttk.Label(root, text="", font=("Arial", 16, "bold"))
+password_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 # Copy to clipboard button
 copy_button = ttk.Button(root, text="Copy to Clipboard", command=copy_to_clipboard)
-copy_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+copy_button.grid(row=0, column=2, padx=10, pady=10)
+
+# Special characters checkbox
+special_chars_var = tk.IntVar(value=1)
+special_chars_checkbox = ttk.Checkbutton(root, text="Include Special Characters", variable=special_chars_var)
+special_chars_checkbox.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+
+# Special characters entry
+special_chars_entry = ttk.Entry(root)
+special_chars_entry.insert(0, "!@#$%^&*+")
+special_chars_entry.grid(row=1, column=1, padx=10, pady=5)
+
+# Uppercase checkbox
+uppercase_var = tk.IntVar(value=1)
+uppercase_checkbox = ttk.Checkbutton(root, text="Include Uppercase", variable=uppercase_var)
+uppercase_checkbox.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+# Length selection
+length_label = tk.Label(root, text="Password Length:")
+length_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+length_entry = ttk.Entry(root)
+length_entry.insert(0, "10")  # Default length
+length_entry.grid(row=3, column=1, padx=10, pady=5)
+
+# Generate password button
+generate_button = ttk.Button(root, text="Generate Password", command=generate_password)
+generate_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
 root.mainloop()
